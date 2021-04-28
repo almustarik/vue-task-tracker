0a7bc76430727df62f3055b6edfd7fbfd5d08d6d
Al Mustarik <almustarik@gmail.com>
2021-04-28 12:37:37 +0600

    added all stuffs

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="onClick()"
+    :style="{ background: color }"
+    class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+            // this.$$emit('delete-task', id)
+            this.$emit('btn-click')
+        }
+    }
+}
+</script>
